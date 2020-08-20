cad4e0f9dea0c9f6e1caf63b36921ce8dc4947a4
mason lester <mscle@dtu.dk>
2020-08-20 17:52:30 +0200

    model improvements

--- globalshippingstock.jl
+++ globalshippingstock.jl
@@ -115,28 +115,40 @@ optimize!(Shipping_stock)
 
 #--------------------
 #OUTPUTS
-
+fuel_fueltype_year=zeros(F,Y)
 for f=1:F
 	for y=1:Y
-		fuel_fueltype_year[f,y] = sum(JuMP.value.(z) for s=1:S)
+		fuel_fueltype_year[f,y] = sum(JuMP.value.(z[f,s,y]) for s=1:S)
 	end
 end
+
+fuel_ship_year=zeros(S,Y)
+for s=1:S
+	for y=1:Y
+		fuel_ship_year[s,y] = sum(JuMP.value.(z[f,s,y]) for f=1:F)
+	end
+end
+
 
 
 #creates dataframes
 ships_bought = DataFrame(transpose(JuMP.value.(x)))
 stock = DataFrame(transpose(JuMP.value.(q)))
 fuel_f_y = DataFrame(transpose(fuel_fueltype_year))
+fuel_s_y = DataFrame(transpose(fuel_ship_year))
 
 #adds headers
 rename!(ships_bought, Ships)
 rename!(stock, Ships)
 rename!(fuel_f_y, fuels)
+rename!(fuel_s_y, Ships)
 
 # write DataFrame out to CSV file
 CSV.write("Results_ships_bought.csv", ships_bought)
 CSV.write("Results_stock.csv", stock)
 CSV.write("Results_fuels_f_y.csv", fuel_f_y)
+CSV.write("Results_fuels_s_y.csv", fuel_s_y)
+
 
 #=
 if termination_status(Shipping_stock) == MOI.OPTIMAL
