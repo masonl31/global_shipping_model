ce2139b60c6d5f1a3e6e2be29d7e586347ef97ea
mason lester <mscle@dtu.dk>
2020-09-24 15:10:07 +0200

    inclusion of lifetime calculations

--- ship_lifetime.jl
+++ ship_lifetime.jl
@@ -1,5 +1,8 @@
-#lifetimes
-lifetime=
-[
-25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25	25
-]
+#lifetime of ship
+
+lifetime = zeros(Int64,1,S)
+base_lifetime = 25
+
+for s=1:S
+    lifetime[s] = 25
+end
