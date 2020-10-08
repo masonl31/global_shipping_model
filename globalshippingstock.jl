eb142ee6c039982bc0e5606189b0e4c8511deede
Mason Scott Lester <mscle@win.dtu.dk>
2020-10-08 11:22:56 +0200

    fixed unit error

--- globalshippingstock.jl
+++ globalshippingstock.jl
@@ -40,9 +40,9 @@ dummy = 3
 Shipping_stock = Model(Gurobi.Optimizer)
 
 #variables
-@variable(Shipping_stock, x[1:S,1:Y] >= 0, Int) #number of ships bought per year
-@variable(Shipping_stock, q[1:S,1:Y] >= 0, Int) #ship stock at end of year Y
-@variable(Shipping_stock, z[1:F,1:S,1:Y] >= 0) #amount of fuel per fueltype, ship, and year
+@variable(Shipping_stock, x[1:S,1:Y] >= 0, Int) #number of ships bought per year [# of ships]
+@variable(Shipping_stock, q[1:S,1:Y] >= 0, Int) #ship stock at end of year Y [# of ships]
+@variable(Shipping_stock, z[1:F,1:S,1:Y] >= 0) #amount of fuel per fueltype, ship, and year [MJ]
 
 
 @objective(Shipping_stock, Min, sum(ship_inv[y,s]*x[s,y] for s=1:S, y=1:Y)*onemega + sum(sum(z[f,s,y] for s=1:S)*fuel_cost[f,y] for f=1:F, y=1:Y))
@@ -54,7 +54,7 @@ Shipping_stock = Model(Gurobi.Optimizer)
 @constraint(Shipping_stock, [t=1:T, y=1:Y], sum(q[s,y]*ship_type_relation[t,s]*average_transport_work[s] for s=1:S) >= Ship_Demands[y,t])
 
 #fuel must be consumed by current ship stock
-@constraint(Shipping_stock, [s=1:S, y=1:Y], sum(z[f,s,y]*ship_eff[f,s] for f=1:F) >= q[s,y]*average_transport_work[y,s] * onegiga)
+@constraint(Shipping_stock, [s=1:S, y=1:Y], sum(z[f,s,y]*ship_eff[f,s] for f=1:F) >= q[s,y]*average_transport_work[y,s])
 
 #emission constraint
 #@constraint(Shipping_stock, [y=1:Y], sum(sum(z[f,s,y] for s=1:S) * fuel_emissions[f] for f=1:F) <= emission_limit[y] * onetera * dummy)
